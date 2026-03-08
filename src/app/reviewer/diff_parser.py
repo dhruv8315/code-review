0aae599d415b612866692d2ffbcb15d927b13a8f
Dhruv Patel <dhruv1947patel@gmail.com>
2026-03-08 19:19:33 -0400

    Remove example/debug block from diff_parser.py
    Remove an unused triple-quoted example block that defined a sample  payload, instantiated DiffParser, and printed the parsed result. This cleans up leftover test/demo code from the end of the file and makes no functional changes to the parser implementation.

--- src/app/reviewer/diff_parser.py
+++ src/app/reviewer/diff_parser.py
@@ -100,11 +100,3 @@ class DiffParser:
         
         return files
 """
-
-
-text = [{'filename': 'hi.py', 'status': 'modified', 'additions': 1, 'deletions': 0, 'changes': 1, 'patch': '@@ -1 +1,2 @@\n print("Hello world")\n+print("dhruv-patch-2 files are added!!!")'},
-        {'filename': 'hit.py', 'status': 'modified', 'additions': 0, 'deletions': 1, 'changes': 1, 'patch': '@@ -1 +1,2 @@\n print("Hello world")\n-print("dhruv-patch-3 files are added!!!")\n+print("dhruv-patch-234 files are added!!!")'}]
-
-parser = DiffParser()
-parsed_diff = parser.diff_parser(text)
-print(parsed_diff)
